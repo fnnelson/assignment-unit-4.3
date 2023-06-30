867fca220a3259f0d76e096ef976c27035e8edd0
Forrest Nelson <forrestnnelson@gmail.com>
2023-06-30 17:03:50 -0500

    based on stretch goals, moved all code around to update isFull and addItem, still figuring out a couple remaining issues with the tests

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,40 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+const maxItems = 5;
+
+//Create a global variable named basket and set it to an empty array. ✅
+
 let basket = [];
 
 console.log(basket);
 
 console.log(`Basket is currently ${basket}`);
 
+function isFull(container) {
+    if (maxItems > container.length) {
+        return false;
+    } else if (maxItems <= container.length) {
+        return true;
+    } else {
+        return "not an array!";
+    }
+}
+
+//Create a function called addItem. It should:
+// take an input parameter for a string item ✅
+// add the new item to the global array basket. ✅
+// return true indicating the item was added ✅ (the tester doesn't like this one anymore now that I updated addItem for the Stretch Goals)
+
+
 function addItem(item) {
-    basket.push(item);
-    return true;
+    if (isFull(basket) == false) {
+        basket.push(item);
+        return true;
+    } else {
+        console.log(`The basket is already full! Cannot add ${item}!`);
+        return false;
+    }
 }
 
 console.log('Adding apples (expect true):', addItem('apples'));
@@ -20,8 +45,6 @@ console.log('Adding crackers (expect true):', addItem('crackers'));
 
 
 console.log(basket);
-
-console.log(`Basket is now ${basket}`);
 
 function listItems(container) {
     for (thing in container) {
@@ -50,38 +73,30 @@ function empty(container) {
 console.log('After removing everything... basket is:', empty(basket));
 console.log('Basket is now:', basket);
 
-console.log("******* Stretch Goals *******");
 
-const maxItems = 5;
-
-function isFull(container) {
-    if (maxItems > container.length) {
-        return false;
-    } else if (maxItems <= container.length) {
-        return true;
-    } else {
-        return "not an array!";
-    }
-}
-
-function addItemCarefully(item) {
-    if (isFull(basket) == false) {
-        basket.push(item);
-        return true;
-    } else {
-        console.log(`The basket is already full! Cannot add ${item}!`);
-        return false;
-    }
-}
-
-console.log('Adding pita chips (expect true):', addItemCarefully('pita chips'));
-console.log('Adding hummus (expect true):', addItemCarefully('hummus'));
-console.log('Adding olives (expect true):', addItemCarefully('olives'));
-console.log('Adding beer (expect true):', addItemCarefully('beer'));
-console.log('Adding tzatziki (expect true):', addItemCarefully('tzatziki'));
-console.log('Adding moussaka (expect false):', addItemCarefully('moussaka'));
+console.log('Adding pita chips (expect true):', addItem('pita chips'));
+console.log('Adding hummus (expect true):', addItem('hummus'));
+console.log('Adding olives (expect true):', addItem('olives'));
+console.log('Adding beer (expect true):', addItem('beer'));
+console.log('Adding tzatziki (expect true):', addItem('tzatziki'));
+console.log('Adding moussaka (expect false):', addItem('moussaka'));
 
 console.log(`Basket is now: ${basket}.`);
+
+function removeItem(item) {
+    if (basket.includes(item)) {
+        basket.splice(basket.indexOf(item), 1);
+        console.log(`Removed ${item}.`)
+        return item;
+    } else {
+        return null;
+    }
+
+}
+
+console.log('Removing beer from basket:', removeItem('beer'));
+console.log('Removing tzatziki from basket:', removeItem('tzatziki'));
+console.log('Removing moussaka from basket:', removeItem('moussaka'));
 
 
 
