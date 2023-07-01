0c9a0238db2713ac354883f9002ddada34364ba2
Forrest Nelson <forrestnnelson@gmail.com>
2023-07-01 14:33:49 -0500

    went through each test to be sure the correct result occurs using code, added notes for each step, added 2nd question in html

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,8 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//Stretch: Add a global const named maxItems and set it to 5. ✅
+
 const maxItems = 5;
 
 //Create a global variable named basket and set it to an empty array. ✅
@@ -12,24 +14,31 @@ console.log(basket);
 
 console.log(`Basket is currently ${basket}`);
 
+//Stretch: Create a function called isFull(). It should:
+// return false if the basket contains less than max number of items ✅
+// return true otherwise (equal or more than maxItems) ✅
+
 function isFull(container) {
-    if (maxItems > container.length) {
+    if (container.length < maxItems) {
         return false;
-    } else if (maxItems <= container.length) {
+    } else if (container.length >= maxItems) {
         return true;
-    } else {
-        return "not an array!";
     }
+    return container;
 }
 
 //Create a function called addItem. It should:
 // take an input parameter for a string item ✅
 // add the new item to the global array basket. ✅
-// return true indicating the item was added ✅ (the tester doesn't like this one anymore now that I updated addItem for the Stretch Goals)
+// return true indicating the item was added ✅ 
 
+//Stretch: Update the required addItem function to:
+// Use the isFull function to prevent more than maxItems from being added to the basket. ✅
+// If an item was added to the array, return true ✅
+// If there was no room and the item could not be added return false ✅
 
 function addItem(item) {
-    if (isFull(basket) == false) {
+    if (isFull(basket) === false) {
         basket.push(item);
         return true;
     } else {
@@ -46,10 +55,15 @@ console.log('Adding crackers (expect true):', addItem('crackers'));
 
 console.log(basket);
 
+//Create a function called listItems. It should:
+// loop over the items in the basket array ✅
+// console.log each individual item on a new line ✅
+
 function listItems(container) {
-    for (thing in container) {
+    for (let thing in container) {
         console.log(container[thing]);
     }
+    return container;
 }
 
 // function listItems(container) {
@@ -63,15 +77,18 @@ function listItems(container) {
 listItems(basket);
 console.log(`Basket items are: ${basket}`);
 
+//Create a function called empty. It should:
+// reset the basket to an empty array ✅
+
 function empty(container) {
     while (container.length > 0) {
         console.log('Taking out:', container.pop());
     }
-    return "empty";
+    return container;
 }
 
-console.log('After removing everything... basket is:', empty(basket));
-console.log('Basket is now:', basket);
+console.log('After removing every item... basket is now:', empty(basket));
+
 
 
 console.log('Adding pita chips (expect true):', addItem('pita chips'));
@@ -82,6 +99,12 @@ console.log('Adding tzatziki (expect true):', addItem('tzatziki'));
 console.log('Adding moussaka (expect false):', addItem('moussaka'));
 
 console.log(`Basket is now: ${basket}.`);
+
+//Stretch: Create a function called removeItem. It should:
+// Take an input parameter for a string item ✅
+// Use Array.indexOf to find the index of the first matching item in the basket. ✅
+// Use Array.splice to remove the first matching item from the basket. ✅
+// Return the item removed or null if the item was not found ✅
 
 function removeItem(item) {
     if (basket.includes(item)) {
